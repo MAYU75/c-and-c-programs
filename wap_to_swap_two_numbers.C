a3523bd6dcc7ca10fafe33b5e71bb2e57d63d8a0
MAYU75 <74301447+MAYU75@users.noreply.github.com>
2020-11-18 16:15:44 +0530

    Update wap_to_swap_two_numbers.C

--- wap_to_swap_two_numbers.C
+++ wap_to_swap_two_numbers.C
@@ -8,8 +8,8 @@ void main()
  scanf("%d%d",&a,&b);
  printf("\n Before swapping");
  printf("\n a=%d and b=%d",a,b);
+ c=a;
  a=b;
  b=c;
- c=a;
  printf("\n After swapping");
  printf("\n a=%d and b=%d",a,b);
